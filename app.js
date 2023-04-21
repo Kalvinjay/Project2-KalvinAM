04206a5bc92c2d5247e24ed72175ac23eba3755a
Kalvinjay <kalvinjaymendez@gmail.com>
2023-04-21 02:17:20 -0500

    fixed pwa part 3

--- app.js
+++ app.js
@@ -10,6 +10,21 @@ app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.get("/manifest.json", function(req, res){
+    res.header("Content-Type", "text/cache-manifest");
+    res.sendFile(path.join(__dirname, "manifest.json"));
+  });
+  
+  app.get("/sw.js", function(req, res){
+    res.header("Content-Type", "text/javascript");
+    res.sendFile(path.join(__dirname, "sw.js"));
+  });
+  
+  app.get("/loader.js", function(req, res){
+    res.header("Content-Type", "text/javascript");
+    res.sendFile(path.join(__dirname, "loader.js"));
+  });
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', routes);
 
--- start.js
+++ start.js
@@ -17,21 +17,6 @@ mongoose.connection
 require('./models/Registration');
 const app = require('./app');
 
-app.get("/manifest.json", function(req, res){
-  res.header("Content-Type", "text/cache-manifest");
-  res.sendFile(path.join(__dirname, "manifest.json"));
-});
-
-app.get("/sw.js", function(req, res){
-  res.header("Content-Type", "text/javascript");
-  res.sendFile(path.join(__dirname, "sw.js"));
-});
-
-app.get("/loader.js", function(req, res){
-  res.header("Content-Type", "text/javascript");
-  res.sendFile(path.join(__dirname, "loader.js"));
-});
-
 const server = app.listen(3000, () => {
   console.log(`Express is running on port ${server.address().port}`);
 });
