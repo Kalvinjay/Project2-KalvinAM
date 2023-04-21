ebdcb2299c4379e458b65b60a9a7a08cb44ab978
Kalvinjay <kalvinjaymendez@gmail.com>
2023-04-21 02:08:58 -0500

    fixed the pwa

--- routes/index.js
+++ routes/index.js
@@ -58,5 +58,20 @@ router.post('/',
              });
           }
     });
+  
+  app.get("/manifest.json", function(req, res){
+    res.header("Content-Type", "text/cache-manifest");
+    res.sendFile(path.join(__dirname, "manifest.json"));
+  });
+
+  app.get("/sw.js", function(req, res){
+    res.header("Content-Type", "text/javascript");
+    res.sendFile(path.join(__dirname, "sw.js"));
+  });
+
+  app.get("/loader.js", function(req, res){
+    res.header("Content-Type", "text/javascript");
+    res.sendFile(path.join(__dirname, "loader.js"));
+  });
 
 module.exports = router;
