2dafa8a0ae1d27a0f4d40360bbe4a786a0e3ebac
Kalvinjay <kalvinjaymendez@gmail.com>
2023-04-21 02:29:40 -0500

    pwa part 6

--- start.js
+++ start.js
@@ -14,8 +14,43 @@ mongoose.connection
     console.log(`Connection error: ${err.message}`);
   });
 
+  app.get("/", (req,res, next)=> {
+    let users=[];
+    axios.get("https://randomuser.me/api/?results=10")
+    .then((response)=>{
+      response.data.results.map((user)=> {
+        users.push(user);
+      });
+      console.log(users);
+      res.render("main", {
+        users
+      });
+    })
+    .catch((err)=> {
+      console.log(err);
+      res.render("main");
+    });
+  });
+
 require('./models/Registration');
 const app = require('./app');
+
+
+pp.get("/manifest.json", function(req, res){
+  res.header("Content-Type", "text/cache-manifest");
+  res.sendFile(path.join(__dirname, "manifest.json"));
+});
+
+app.get("/sw.js", function(req, res){
+  res.header("Content-Type", "text/javascript");
+  res.sendFile(path.join(__dirname, "sw.js"));
+});
+
+app.get("/loader.js", function(req, res){
+  res.header("Content-Type", "text/javascript");
+  res.sendFile(path.join(__dirname, "loader.js"));
+});
+
 
 const server = app.listen(3000, () => {
   console.log(`Express is running on port ${server.address().port}`);
