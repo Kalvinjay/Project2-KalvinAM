eaa2672c500b568879817996dc058c9f9021b597
Kalvinjay <kalvinjaymendez@gmail.com>
2023-04-21 02:32:52 -0500

    part 8 pwa reset

--- start.js
+++ start.js
@@ -14,42 +14,42 @@ mongoose.connection
     console.log(`Connection error: ${err.message}`);
   });
 
-  app.get("/", (req,res, next)=> {
-    let users=[];
-    axios.get("https://randomuser.me/api/?results=10")
-    .then((response)=>{
-      response.data.results.map((user)=> {
-        users.push(user);
-      });
-      console.log(users);
-      res.render("main", {
-        users
-      });
-    })
-    .catch((err)=> {
-      console.log(err);
-      res.render("main");
-    });
-  });
+  // app.get("/", (req,res, next)=> {
+  //   let users=[];
+  //   axios.get("https://randomuser.me/api/?results=10")
+  //   .then((response)=>{
+  //     response.data.results.map((user)=> {
+  //       users.push(user);
+  //     });
+  //     console.log(users);
+  //     res.render("main", {
+  //       users
+  //     });
+  //   })
+  //   .catch((err)=> {
+  //     console.log(err);
+  //     res.render("main");
+  //   });
+  // });
 
 require('./models/Registration');
 const app = require('./app');
 
 
-app.get("/manifest.json", function(req, res){
-  res.header("Content-Type", "text/cache-manifest");
-  res.sendFile(path.join(__dirname, "manifest.json"));
-});
+// app.get("/manifest.json", function(req, res){
+//   res.header("Content-Type", "text/cache-manifest");
+//   res.sendFile(path.join(__dirname, "manifest.json"));
+// });
 
-app.get("/sw.js", function(req, res){
-  res.header("Content-Type", "text/javascript");
-  res.sendFile(path.join(__dirname, "sw.js"));
-});
+// app.get("/sw.js", function(req, res){
+//   res.header("Content-Type", "text/javascript");
+//   res.sendFile(path.join(__dirname, "sw.js"));
+// });
 
-app.get("/loader.js", function(req, res){
-  res.header("Content-Type", "text/javascript");
-  res.sendFile(path.join(__dirname, "loader.js"));
-});
+// app.get("/loader.js", function(req, res){
+//   res.header("Content-Type", "text/javascript");
+//   res.sendFile(path.join(__dirname, "loader.js"));
+// });
 
 
 const server = app.listen(3000, () => {
