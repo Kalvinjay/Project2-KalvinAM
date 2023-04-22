98394bcd9096a9467b4b9584e934b66768d053b0
Kalvinjay <kalvinjaymendez@gmail.com>
2023-04-22 10:52:02 -0500

    pwa-demo final rest

--- sw.js
+++ sw.js
@@ -13,7 +13,6 @@ self.addEventListener('install', function(e) {
       return cache.addAll(filesToCache);
     })
   );
-  self.skipWaiting();
 });
 
 // /* Serve cached content when offline */
@@ -24,70 +23,3 @@ self.addEventListener('fetch', function(e) {
     })
   );
 });
-// self.addEventListener('install', function(event) {
-//   console.info('Event: Install');
-//   event.waitUntil(
-//     caches.open("static-cache")
-//     .then((cache) => {
-//       return cache.addAll([
-//         "./"
-//       ])
-//       .then(() => {
-//         console.info('All files are cached');
-//         return self.skipWaiting();
-//       })
-//       .catch((error) => {
-//         console.error('Failed to cache', error);
-//       })
-//     })
-//   );
-// });
-
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
-// self.addEventListener('fetch', (event) => {
-//   console.info('Event: Fetch');
-
-//   var request = event.request;
-
-//   event.respondWith(
-//     caches.match(request).then((response) => {
-//       if (response) {
-//         return response;
-//       }
-
-//       return fetch(request).then((response) => {
-//         var responseToCache = response.clone();
-//         caches.open("static-cache").then((cache) => {
-//           cache.put(request, responsibleToCache).catch((err) => {
-//             console.warn(request.url + ': ' + err.message);
-//           });
-//         });
-
-//         return response;
-//       })
-//     })
-//   )
-// })
-
-// self.addEventListener('activate', (event) => {
-//   console.info('Event: Activate');
-
-//   event.waitUntil(
-//     caches.keys().then((cacheNames) => {
-//       return Promise.all(
-//         cacheNames.map((cache) => {
-//           if (cache !== "static-cache") {
-//             return caches.delete(cache);
-//           }
-//         })
-//       )
-//     })
-//   )
-// })
